3a1a49766520733f9c755d2f0ef54eb9c19fbede
Christopher Vazquez <c_vazquez08@yahoo.com>
2019-03-27 19:18:04 -0400

    life cycle notes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+// App.js
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isRunning: true };
+    console.log('I am in the CONSTRUCTOR of the App.js.');
+  }
+
+  stopCounter = () => {
+      this.setState({...this.state, isRunning: false })
+  }
+
   render() {
+    console.log('I am in RENDER of the App.js.');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Clicking this button will destroy the Counter component and clear the state.</h2>
+        <button onClick={this.stopCounter}>Stop the counter</button>
+
+       {this.state.isRunning && <Counter />}  
+       
       </div>
-    );
+    )
   }
 }
 
